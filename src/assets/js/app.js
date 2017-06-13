027e70a9ea96dde7e16f871c117a52ca720c5af7
stolenhead <andrea.palomino.ruby@gmail.com>
2017-06-13 11:24:36 -0500

    queries

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -55,27 +55,9 @@ $(document).ready(function(){
     initializeCharts();
   });
 
-  var navPrimary = document.querySelector(".nav--primary");
-  var navItems = document.querySelectorAll(".nav-item--primary");
-
-  navItems.forEach(function(item) {
-    var secondary = item.querySelector(".nav--secondary");
-    if (secondary) {
-      // secondary.style.height = secondary.offsetHeight + "px";
-      secondary.style.height = "auto";
-    }
-    item.classList.add("is-collapsed");
-
-    item.onclick = function() {
-      item.classList.toggle("is-collapsed");
-      item.classList.toggle("is-active");
-    };
-  });
-
   $('.collapse__head').click(function(){
     $(this).toggleClass('open'); 
     $(this).parent().find('.collapse__body').slideToggle('slow');
     $(this).find("path").toggleClass("flecha");
-    // $('path').toggleClass('flecha');
   });
 })
